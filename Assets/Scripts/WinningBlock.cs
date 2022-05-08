18ef3674e285d6ee685d592f86f59391e7572993
Eivin Sellevold <73343666+EivinSS@users.noreply.github.com>
2022-05-08 16:17:44 +0200

    Raise winning block. When coins are collected, we lower WinningBlock, and can collect the sphere

--- Assets/Scripts/WinningBlock.cs
+++ Assets/Scripts/WinningBlock.cs
@@ -6,6 +6,7 @@ public class WinningBlock : MonoBehaviour
 {
     [SerializeField] Material winningMat;
     [SerializeField] GameObject winningSphere;
+    [SerializeField] Vector3 toPosition;
 
     private void Start()
     {
@@ -15,10 +16,32 @@ public class WinningBlock : MonoBehaviour
     {
         SetWinningMat();
         winningSphere.SetActive(true);
+        MoveBlock();
     }
 
     void SetWinningMat()
     {
         GetComponent<Renderer>().material = winningMat;
+    }
+
+    void MoveBlock()
+    {
+        Vector3 toPos = toPosition;
+        GameManager.Instance.TellGMMoveStatus(gameObject.name, true, true);
+        StartCoroutine(MoveToPos(toPos));
+    }
+
+    IEnumerator MoveToPos(Vector3 movingToTarget)
+    {
+        Vector3 diffVector = movingToTarget - transform.position;
+        Vector3 stepDistance = Vector3.Scale(diffVector, new Vector3(0.025f, 0.025f, 0.025f));
+        while (Vector3.Distance(transform.position, movingToTarget) >= 0.01f)
+        {
+            Vector3 newPos = new Vector3(transform.position.x + stepDistance.x, transform.position.y + stepDistance.y, transform.position.z + stepDistance.z);
+            transform.position = newPos;
+            yield return new WaitForSeconds(0.02f);
+        }
+        GameManager.Instance.TellGMMoveStatus(gameObject.name, false, true);
+        yield break;
     }
 }
