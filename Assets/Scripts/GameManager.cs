761846f19d12eda6e2738bacbff5e864d9c9940d
Eivin Sellevold <73343666+EivinSS@users.noreply.github.com>
2022-11-19 09:19:33 +0100

    removing using system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
-using System;
 using System.Linq;
 
 public class GameManager : MonoBehaviour
