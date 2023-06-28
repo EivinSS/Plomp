ae20d4ecb8e8e668c73a03eec05995a3a5637594
Eivin Sellevold <73343666+EivinSS@users.noreply.github.com>
2023-06-28 19:45:16 +0200

    renaming files. AND making breaking blocks fall

--- Assets/Scripts/BreakingBlockRegular.cs
+++ Assets/Scripts/BreakingBlockRegular.cs
@@ -18,7 +18,9 @@ public class BreakingBlockRegular : Block
         DecrementTimesBeforeBreaking();
         if (CheckIfBreak())
         {
-            Destroy(gameObject);
+            StartCoroutine(MoveFall());
+            FadeOutMaterials();
+            Destroy(gameObject, 2); ;
         }
     }
 
@@ -35,5 +37,43 @@ public class BreakingBlockRegular : Block
             return true;
         }
         return false;
+    }
+
+    public IEnumerator MoveFall()
+    {
+        Vector3 movingToTarget;
+        float moveSpeedAll = 10;
+        movingToTarget = new Vector3(transform.position.x, transform.position.y - 20f, transform.position.z);
+        while (Vector3.Distance(transform.position, movingToTarget) >= 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, movingToTarget, Time.deltaTime * moveSpeedAll);
+            yield return null;
+        }
+        yield break;
+    }
+
+    public virtual void FadeOutMaterials()
+    {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+        foreach (Material m in renderer.materials)
+        {
+            StartCoroutine(FadeOutMaterial(m));
+        }
+    }
+
+    IEnumerator FadeOutMaterial(Material m)
+    {
+        float elapsedTime = 0f;
+        Color initialColor = m.color;
+        Color targetColor = new Color(initialColor.r, initialColor.g, initialColor.b, 0f);
+
+        while (elapsedTime < 2f)
+        {
+            elapsedTime += 0.01f;
+            m.color = Color.Lerp(initialColor, targetColor, elapsedTime / 2f);
+            yield return new WaitForSeconds(0.005f);
+        }
+        yield return null;
     }
 }
